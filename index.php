1807b6efe549334df1422a1a0e7a2b31b5e43911
Rifqi Azhar Nugraha <rifqi.an@gmail.com>
2022-10-20 08:12:31 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dokumentasi Siswa XII RPL</title>
+</head>
+<body>
+	<h1>Selamat Datang di XII RPL</h1>
+</body>
+</html>
